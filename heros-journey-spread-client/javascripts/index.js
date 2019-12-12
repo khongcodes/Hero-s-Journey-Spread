110cb743624582dca11086021323b78f8ead9ad7
KHong <albtrossfacefall@gmail.com>
2019-12-11 20:39:50 -0500

    started adding seeds

---
+++ heros-journey-spread-client/javascripts/index.js
@@ -0,0 +1,9 @@
+
+// Pull card image based on api
+// fetch('http://localhost:3000/cards/1')
+// .then(resp => resp.json())
+// .then(obj => function() {
+// 	let card1 = document.createElement('img');
+// 	card1.src = `assets/card-images/${obj.card_type}/${obj.value}.jpg`;
+// 	document.body.appendChild(card1);
+// }())
