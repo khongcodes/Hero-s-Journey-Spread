26fa360478f0cce1af734020c08d668e62bbb161
KHong <albtrossfacefall@gmail.com>
2019-12-13 10:31:12 -0500

    begin fleshing out journey CRUD

--- heros-journey-spread-api/app/controllers/journeys_controller.rb
+++ heros-journey-spread-api/app/controllers/journeys_controller.rb
@@ -1,2 +1,15 @@
 class JourneysController < ApplicationController
+  def index
+    journeys = Journey.all
+    render json: journeys
+  end
+
+  def show
+    journey = Journey.find(params[:id])
+    render json: journey
+  end
+
+  def create
+    journey = Journey.create(name:params[:name])
+  end
 end
