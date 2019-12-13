b4da39e0bf73a5395d6710a7aae6e2c8ff231254
KHong <albtrossfacefall@gmail.com>
2019-12-13 13:18:11 -0500

    basic index.html layout

--- heros-journey-spread-client/javascripts/index.js
+++ heros-journey-spread-client/javascripts/index.js
@@ -1,14 +1,14 @@
 
 // Pull card image based on api
-fetch('http://localhost:3000/cards/random/1')
-.then(resp => resp.json())
-.then(obj => function() {
-	let card1 = document.createElement('img');
-  if (obj[0].card_type === 'major') {
-    card1.src = `assets/card-images/${obj[0].card_type}/${obj[0].value}.jpg`;
-  } else {
-    card1.src = `assets/card-images/${obj[0].card_type}/${obj[0].suit}/${obj[0].value}.jpg`;
-  }
+// fetch('http://localhost:3000/cards/random/1')
+// .then(resp => resp.json())
+// .then(obj => function() {
+// 	let card1 = document.createElement('img');
+//   if (obj[0].card_type === 'major') {
+//     card1.src = `assets/card-images/${obj[0].card_type}/${obj[0].value}.jpg`;
+//   } else {
+//     card1.src = `assets/card-images/${obj[0].card_type}/${obj[0].suit}/${obj[0].value}.jpg`;
+//   }
   
-	document.body.appendChild(card1);
-}());
+// 	document.body.appendChild(card1);
+// }());
