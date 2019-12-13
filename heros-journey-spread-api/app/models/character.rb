e82a67008d13168b30cee1529a4f6b84651fe963
KHong <albtrossfacefall@gmail.com>
2019-12-13 10:34:16 -0500

    added dependent destroy for Points on Character and Journey

--- heros-journey-spread-api/app/models/character.rb
+++ heros-journey-spread-api/app/models/character.rb
@@ -1,5 +1,5 @@
 class Character < ApplicationRecord
-  has_many :points, as: :querent
+  has_many :points, as: :querent, dependent: :destroy
   has_many :cards, through: :points
   # has_many :journeys, optional: true
 end
--- heros-journey-spread-api/app/models/journey.rb
+++ heros-journey-spread-api/app/models/journey.rb
@@ -1,5 +1,5 @@
 class Journey < ApplicationRecord
-  has_many :points, as: :querent
+  has_many :points, as: :querent, dependent: :destroy
   has_many :cards, through: :points
   # belongs_to :character
 end
