17cd70f67d9d1196e8fbfaf0a621d089a95c4d47
KHong <albtrossfacefall@gmail.com>
2019-12-17 16:02:06 -0500

    load menu switches

--- heros-journey-spread-client/javascripts/load-menu.js
+++ heros-journey-spread-client/javascripts/load-menu.js
@@ -7,7 +7,13 @@ const loadMenuOpens = function() {
 
   const inactiveContainer = document.createElement('div');
   inactiveContainer.className = 'load-menu inactive-container';
-
+  
+  const inactiveLabel = document.createElement('span');
+  inactiveLabel.className = 'load-menu inactive-label';
+  inactiveLabel.innerText = `Load ${(activeLoadMenuType === 'character' ? 'Journey' : 'Character')}`
+  inactiveLabel.addEventListener('click', switchActiveMenu);
+  
+  inactiveContainer.appendChild(inactiveLabel);
   loadMenuContainer.append(activeContainer, inactiveContainer);
   document.getElementById('view-wrapper').prepend(loadMenuContainer);
   document.querySelector('div.load-menu.tab.inactive').className = 'load-menu tab active';
@@ -16,4 +22,9 @@ const loadMenuOpens = function() {
 const loadMenuCloses = function() {
   document.getElementById('view-wrapper').removeChild(document.querySelector('.load-menu.menu-container'));
   document.querySelector('div.load-menu.tab.active').className = 'load-menu tab inactive';
+}
+
+const switchActiveMenu = function() {
+  document.querySelector('.load-menu.inactive-label').innerText = `Load ${activeLoadMenuType[0].toUpperCase() + activeLoadMenuType.slice(1)}`;
+  activeLoadMenuType = (activeLoadMenuType === 'character' ? 'journey' : 'character');
 }
