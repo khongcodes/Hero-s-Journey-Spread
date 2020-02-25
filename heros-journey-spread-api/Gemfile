e76a227154bb8c5003ef929b9137bd44fda5f632
KHong <albtrossfacefall@gmail.com>
2020-02-25 09:32:22 -0500

    switch over to postgres database for heroku deployment

--- heros-journey-spread-api/Gemfile
+++ heros-journey-spread-api/Gemfile
@@ -9,7 +9,11 @@ gem 'rails', '~> 6.0.1'
 gem "rack", ">= 2.0.8"
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+# gem 'sqlite3', '~> 1.4'
+
+# Use postgresql as database for heroku deployment
+gem 'pg'
+
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- heros-journey-spread-api/db/schema.rb
+++ heros-journey-spread-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_13_143747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.string "card_type"
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 2019_12_13_143747) do
   end
 
   create_table "cards_points", id: false, force: :cascade do |t|
-    t.integer "card_id"
-    t.integer "point_id"
+    t.bigint "card_id"
+    t.bigint "point_id"
     t.index ["card_id"], name: "index_cards_points_on_card_id"
     t.index ["point_id"], name: "index_cards_points_on_point_id"
   end
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_143747) do
 
   create_table "journeys", force: :cascade do |t|
     t.string "name"
-    t.integer "character_id"
+    t.bigint "character_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["character_id"], name: "index_journeys_on_character_id"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_143747) do
     t.string "querent_ref"
     t.string "description"
     t.string "querent_type"
-    t.integer "querent_id"
+    t.bigint "querent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["querent_type", "querent_id"], name: "index_points_on_querent_type_and_querent_id"
